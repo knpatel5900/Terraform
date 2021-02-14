44cebb04b22dfc186c4c4dd341303d7fc5ec07a1
Kishan Patel <kpatel@savanacorp.net>
2021-02-14 18:22:20 -0500

    ccdd

--- vpc.tf
+++ vpc.tf
@@ -7,33 +7,6 @@ resource "aws_vpc" "Terraform_VPC" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.Terraform_VPC.id
-  cidr_block              = var.subnetCIDRblock_public
-  map_public_ip_on_launch = var.mapPublicIP
-  availability_zone       = var.availabilityZone
-tags = {
-   Name = "public_subnet"
-}
-}
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = aws_vpc.Terraform_VPC.id
-  cidr_block              = var.subnetCIDRblock_private
-  map_public_ip_on_launch = var.mapPublicIP
-  availability_zone       = var.availabilityZone
-tags = {
-   Name = "private_subnet"
-}
-resource "aws_subnet" "my-subnet" {
-  vpc_id                  = aws_vpc.Terraform_VPC.id
-  cidr_block              = var.subnetCIDRblock_my
-  map_public_ip_on_launch = var.mapPublicIP
-  availability_zone       = var.availabilityZone
-tags = {
-   Name = "my-subnet"
-}
-
-
 }
 
 output "Name" {
@@ -44,24 +17,6 @@ output "Name" {
 output "cidr" {
   value = aws_vpc.Terraform_VPC.cidr_block
   description = "This is CIDR_Block"
-}
-
-output "public_subnet_name" {
-  value = aws_subnet.public_subnet.tags
-  description = "This is Public_Subnet"
-}
-output "public_subnet_AZ" {
-  value = aws_subnet.public_subnet.availability_zone
-  description = "This is AZ"
-}
-
-output "private_subnet_name" {
-  value = aws_subnet.private_subnet.tags
-  description = "This is Public_Subnet"
-}
-output "private_subnet_AZ" {
-  value = aws_subnet.private_subnet.availability_zone
-  description = "This is AZ"
 }
 
 
