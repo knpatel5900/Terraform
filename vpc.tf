b9f874b80892d98b54be4500f3aad95412b0c176
Kishan Patel <kpatel@savanacorp.net>
2021-02-14 18:28:52 -0500

    final commit

--- vpc.tf
+++ vpc.tf
@@ -7,6 +7,33 @@ resource "aws_vpc" "Terraform_VPC" {
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.Terraform_VPC.id
+  cidr_block              = var.subnetCIDRblock_public
+  map_public_ip_on_launch = var.mapPublicIP
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "public_subnet"
+}
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.Terraform_VPC.id
+  cidr_block              = var.subnetCIDRblock_private
+  map_public_ip_on_launch = var.mapPublicIP
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "private_subnet"
+}
+resource "aws_subnet" "my-subnet" {
+  vpc_id                  = aws_vpc.Terraform_VPC.id
+  cidr_block              = var.subnetCIDRblock_my
+  map_public_ip_on_launch = var.mapPublicIP
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "my-subnet"
+}
+
+
 }
 
 output "Name" {
