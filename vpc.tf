e61990499f367b66f8d28e888780194431add8ce
Kishan Patel <kpatel@savanacorp.net>
2021-02-14 20:31:13 -0500

    kp subnet

--- vpc.tf
+++ vpc.tf
@@ -35,6 +35,16 @@ tags = {
 }
 }
 
+resource "aws_subnet" "kp_subnet" {
+  vpc_id                  = aws_vpc.Terraform_VPC.id
+  cidr_block              = var.subnetCIDRblock_remote
+  map_public_ip_on_launch = var.mapPublicIP
+  availability_zone       = var.availabilityZone
+tags = {
+   Name = "kp_subnet"
+}
+}
+
 output "Name" {
   value = aws_vpc.Terraform_VPC.tags
    description  = "This Name of VPC"
